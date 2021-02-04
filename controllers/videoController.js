7214098df01c8a22e2bef844284538a2d89700f8
sbtiffanykim <sbtiffanykim@gmail.com>
2021-02-04 12:32:34 +0900

    Configure a feature to upload a video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,15 @@
 import routes from "../routes";
+import Video from "../models/Video";
 
-export const home = (req, res) =>
-  res.render("home", { pageTitle: "Home", videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch(error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos:[] });
+  }
+}
 
 export const search = (req, res) => {
   const {
@@ -14,12 +22,18 @@ export const search = (req, res) => {
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
 
-export const postUpload = (req, res) => {
-  const {
-    body: { file, title, description },
+export const postUpload = async(req, res) => {
+  const { 
+    body: { title, description }, 
+    file: { path } 
   } = req;
-  // To Do: Upload and save video
-  res.redirect(routes.videoDetail(32345));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title, 
+    description
+  });
+  console.log(newVideo);
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) =>
